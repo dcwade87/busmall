171a7a39a3c824753db2f8c0b1710f7a608e9ce6
Daniel Wade <dcwade87@gmail.com>
2016-02-06 12:55:46 -0800

    added a math function and worked on displaying pics through array

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ var voteRounds = 0
 
 function ImageVoter(name) {
   this.path = "img/" + name + ".jpg";
-  this.name = name
-  this.imgvote = 0;
+  this.name = name;
+  this.imgVote = 0;
   imgArr.push(this);
 };
 
@@ -14,42 +14,19 @@ function generatePic() {
     new ImageVoter(imgName[name]);
   };
 };
-
 generatePic();
 
+randomNumber = function() {
+  return Math.floor((Math.random() * (15 - 1) + 1));
+}
+
 var img = document.createElement("img");
-img.src = "img/cthulhu.jpg";
+img.src = imgArr[6].path;
 
 var src = document.getElementById("vote1");
 src.appendChild(img);
 
-var img = document.createElement("img");
-img.src = "img/shark.jpg";
-
-var src = document.getElementById("vote2");
-src.appendChild(img);
-
-var img = document.createElement("img");
-img.src = "img/dragon.jpg";
-
-var src = document.getElementById("vote3");
-src.appendChild(img);
-
 vote1.addEventListener("click", function() {
-  event.preventDefault();
-  ImageVoter.addvote += 1;
-  voteRounds += 1;
-  console.log("Added Vote Point");
-});
-
-vote2.addEventListener("click", function() {
-  event.preventDefault();
-  ImageVoter.addvote += 1;
-  voteRounds += 1;
-  console.log("Added Vote Point");
-});
-
-vote3.addEventListener("click", function() {
   event.preventDefault();
   ImageVoter.addvote += 1;
   voteRounds += 1;
